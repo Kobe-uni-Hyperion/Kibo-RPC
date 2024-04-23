7ce03cb75ff8f6f64a962f8d58a8a7938d7295dd
yuuki <yuuki@asa.local>
2024-04-23 13:30:49 +0900

    add template files

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -0,0 +1,26 @@
+package jp.jaxa.iss.kibo.rpc.defaultapk;
+
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
+/**
+ * Class meant to handle commands from the Ground Data System and execute them in Astrobee
+ */
+
+public class YourService extends KiboRpcService {
+    @Override
+    protected void runPlan1(){
+        // write your plan 1 here
+    }
+
+    @Override
+    protected void runPlan2(){
+        // write your plan 2 here
+    }
+
+    @Override
+    protected void runPlan3(){
+        // write your plan 3 here
+    }
+
+}
+
