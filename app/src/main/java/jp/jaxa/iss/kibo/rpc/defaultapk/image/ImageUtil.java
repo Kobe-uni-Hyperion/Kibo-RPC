e4be34cddbf9f2e48ba2b90714347248d5ec9b0a
RenyaGit <wadarenya1221@gmail.com>
2024-06-18 23:39:21 +0900

    fix: srcPointsとdstPointのタイプをCV_32Fに変換

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/image/ImageUtil.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/image/ImageUtil.java
@@ -3,6 +3,7 @@ package jp.jaxa.iss.kibo.rpc.defaultapk.image;
 import org.jetbrains.annotations.Nullable;
 import org.opencv.aruco.Aruco;
 import org.opencv.aruco.Dictionary;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
@@ -47,12 +48,16 @@ public class ImageUtil {
         Mat transformMatrix;
         {
             MatOfPoint2f srcPoints = new MatOfPoint2f(points);
+            srcPoints.convertTo(srcPoints, CvType.CV_32F); // タイプをCV_32Fに変換
+
             MatOfPoint2f dstPoint = new MatOfPoint2f(
                     new Point(0, 0),
                     new Point(width - 1, 0),
                     new Point(width - 1, height - 1),
                     new Point(0, height - 1)
             );
+            dstPoint.convertTo(dstPoint, CvType.CV_32F); // タイプをCV_32Fに変換
+
             // 変換前の座標と変換後の座標から透視変換行列(切り抜きたい領域を長方形に変換するための行列)を作る
             transformMatrix = Imgproc.getPerspectiveTransform(srcPoints, dstPoint);
         }
@@ -65,6 +70,7 @@ public class ImageUtil {
 
     @Nullable
     public static Mat clipAR(Mat image) {
+        // ARタグを検知する
         Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
         ArrayList<Mat> corners = new ArrayList<>();
         Mat markerIds = new Mat();
@@ -72,4 +78,5 @@ public class ImageUtil {
         if (corners.isEmpty()) return null;
         return clip(image, corners.get(0));
     }
+
 }
